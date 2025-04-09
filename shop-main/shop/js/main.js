576d8bb6d2eca8a667c6aadd60f2dd9e48d2cafd
dkanrjsk7456 <syc0105229@naver.com>
2025-04-10 00:10:52 +0900

    0410/0008

--- shop-main/shop/js/main.js
+++ shop-main/shop/js/main.js
@@ -115,11 +115,7 @@
 	  ],
 	});
   
-	// Product img zoom
-	var zoomMainProduct = document.getElementById("product-main-img");
-	if (zoomMainProduct) {
-	  $("#product-main-img .product-preview").zoom();
-	}
+	
   
 	/////////////////////////////////////////
   
