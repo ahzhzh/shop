6fa761264933b9aeeb008e5ef3b9dd5be9d3e2ff
dkanrjsk7456 <syc0105229@naver.com>
2025-04-28 12:29:06 +0900

    0428 / 1230

--- shop-main/shop/js/products.js
+++ shop-main/shop/js/products.js
@@ -2,32 +2,260 @@
 
 const products = {
     "a": {
-        name: "갤럭시 GALAX 지포스 RTX 4070 Ti SUPER EX GAMER WHITE OC D6X 16GB",
-        price: "₩1,915,800",
-        image: "./img/product00.png",
-        specs: [
-            { 항목: "모델명", 세부사항: "갤럭시 GALAX 지포스 RTX 4070 Ti SUPER EX GAMER WHITE OC D6X 16GB" },
-            { 항목: "칩셋", 세부사항: "NVIDIA RTX 4070 Ti SUPER" },
-            { 항목: "GPU 제조 공정", 세부사항: "4nm" },
-            { 항목: "베이스 클럭", 세부사항: "2340MHz" },
-            { 항목: "부스트 클럭", 세부사항: "2640MHz" },
-            { 항목: "OC 클럭", 세부사항: "2655MHz" },
-            { 항목: "스트림 프로세서", 세부사항: "8448개" },
-            { 항목: "인터페이스", 세부사항: "PCIe 4.0 x16" },
-            { 항목: "메모리 종류", 세부사항: "GDDR6X" },
-            { 항목: "메모리 용량", 세부사항: "16GB" },
-            { 항목: "출력 단자", 세부사항: "HDMI2.1 x1, DP1.4 x3" },
-            { 항목: "지원 정보", 세부사항: "8K 지원, HDR 지원, HDCP 2.3, 제로팬(0-dB 기술)" },
-            { 항목: "사용 전력", 세부사항: "285W" },
-            { 항목: "권장 파워 용량", 세부사항: "정격파워 750W 이상" },
-            { 항목: "전원 포트", 세부사항: "16핀 (12VHPWR) x1" },
-            { 항목: "크기 (길이)", 세부사항: "323mm" },
-            { 항목: "두께", 세부사항: "60mm" },
-            { 항목: "쿨링 방식 및 팬 개수", 세부사항: "3팬, 제로팬 기술, LED 라이트 탑재, 백플레이트 포함, XTREME TUNER 지원" },
-            { 항목: "A/S 보증 기간", 세부사항: "3년 무상 보증" }
+    name: "갤럭시 GALAX 지포스 RTX 4070 Ti SUPER EX GAMER WHITE OC D6X 16GB",
+    price: "₩1,915,800",
+    image: "./img/product00.png",
+    desc: [
+        "갤럭시 GALAX 지포스 RTX 4070 Ti SUPER EX GAMER WHITE OC D6X 16GB",
+        "NVIDIA RTX 4070 Ti SUPER",
+        "4nm",
+        "2340MHz",
+        "2640MHz",
+        "2655MHz",
+        "8448개",
+        "PCIe 4.0 x16",
+        "GDDR6X",
+        "16GB",
+        "HDMI2.1 x1, DP1.4 x3",
+        "8K 지원, HDR 지원, HDCP 2.3, 제로팬(0-dB 기술)",
+        "285W",
+        "정격파워 750W 이상",
+        "16핀 (12VHPWR) x1",
+        "323mm",
+        "60mm",
+        "3팬, 제로팬 기술, LED 라이트 탑재, 백플레이트 포함, XTREME TUNER 지원",
+        "3년 무상 보증"
+    ]
+},
+    "b": {
+        name: "이엠텍 지포스 RTX 5070 MIRACLE WHITE D7 12GB",
+        price: "₩1,044,000",
+        image: "./img/product01.png",
+        desc: [
+            "이엠텍 지포스 RTX 5070 MIRACLE WHITE D7 12GB",
+            "NVIDIA RTX 5070",
+            "4nm",
+            "2325MHz",
+            "2512MHz",
+            "6144개",
+            "PCIe 5.0 x16",
+            "GDDR7",
+            "12GB",
+            "192bit",
+            "HDMI2.1 x1, DP2.1 x3",
+            "8K 지원, HDR 지원, HDCP 2.3, 제로팬(0-dB 기술)",
+            "정격파워 650W 이상",
+            "16핀 (12V2x6) x1",
+            "329mm",
+            "45mm",
+            "3팬, 제로팬 기술, LED 라이트 탑재, 백플레이트 포함, DrMOS 기술 적용",
+            "3년보증"
         ]
-    }
-    // 추가 제품은 여기에 계속 추가하면 됩니다.
+    },
+    "c": {
+        name: "MSI 지포스 RTX 5080 뱅가드 SOC D7 16GB 하이퍼프로져",
+        price: "₩2,293,200",
+        image: "./img/product02.png",
+        desc: [
+            "MSI 지포스 RTX 5080 뱅가드 SOC D7 16GB 하이퍼프로져",
+            "NVIDIA RTX 5080",
+            "TSMC 4nm",
+            "2730MHz",
+            "2745MHz",
+            "10752개",
+            "PCIe 5.0 x16",
+            "GDDR7",
+            "16GB",
+            "HDMI2.1 x1, DP2.1 x3",
+            "8K 지원, HDR 지원, HDCP 2.3, 제로팬(0-dB 기술)",
+            "360W",
+            "정격파워 850W 이상",
+            "16핀 (12V2x6) x1",
+            "357mm",
+            "66mm",
+            "3팬, 제로팬 기술, LED 라이트 탑재, 백플레이트 포함, DrMOS 기술 적용, Dual BIOS 지원, MYSTIC LIGHT 기능 포함",
+            "3년 무상 보증"
+        ]
+    },
+    "d": {
+        name: "갤럭시 GALAX 지포스 RTX 5070 EX GAMER WHITE OC D7 12GB",
+        price: "₩1,099,900",
+        image: "./img/product03.png",
+        desc: [
+            "갤럭시 GALAX 지포스 RTX 5070 EX GAMER WHITE OC D7 12GB",
+            "NVIDIA RTX 5070",
+            "4nm",
+            "2325MHz",
+            "2557MHz",
+            "6144개",
+            "PCIe 5.0 x16",
+            "GDDR7",
+            "12GB",
+            "HDMI2.1 x1, DP2.1 x3",
+            "8K 지원, HDR 지원, HDCP 2.3, 제로팬(0-dB 기술)",
+            "250W",
+            "정격파워 650W 이상",
+            "16핀 (12V2x6) x1",
+            "322mm",
+            "52mm",
+            "3팬, 제로팬 기술, LED 라이트 탑재, 백플레이트 포함",
+            "3년 무상 보증"
+        ]
+    },
+    "e": {
+        name: "MSI 지포스 RTX 5070 게이밍 트리오 OC D7 12GB 트라이프로져4",
+        price: "₩1,148,960",
+        image: "./img/product04.png",
+        desc: [
+            "MSI 지포스 RTX 5070 게이밍 트리오 OC D7 12GB 트라이프로져4",
+            "NVIDIA RTX 5070",
+            "4nm",
+            "2325MHz",
+            "2610MHz",
+            "2625MHz",
+            "6144개",
+            "PCIe 5.0 x16",
+            "GDDR7",
+            "12GB",
+            "HDMI2.1 x1, DP2.1 x3",
+            "8K 지원, HDR 지원, HDCP 2.3, 제로팬(0-dB 기술)",
+            "250W",
+            "정격파워 650W 이상",
+            "16핀 (12V2x6) x1",
+            "338mm",
+            "50mm",
+            "3팬, 제로팬 기술, 백플레이트 포함, DrMOS 기술 적용, LED 라이트 탑재, MYSTIC LIGHT 기능 포함",
+            "3년 무상 보증"
+        ]
+    },
+    "f": {
+    name: "인텔 코어 울트라7 시리즈2 265K (애로우레이크)",
+    price: "₩539,990",
+    image: "./img/product05.png",
+    desc: [
+        "LGA 1851",
+        "P코어 8개 + E코어 12개 (총 20스레드)",
+        "DDR5",
+        "탑재 (인텔 그래픽스 Xe LPG)",
+        "TSMC 3nm",
+        "3.9GHz",
+        "5.5GHz",
+        "36MB",
+        "30MB",
+        "125-250W",
+        "PCIe 5.0, PCIe 4.0",
+        "6400MHz",
+        "인텔 XTU, 인텔 딥러닝 부스트",
+        "미포함",
+        "2304점",
+        "36309점",
+        "$394 (VAT 별도)"
+    ]
+},
+    "g": {
+        name: "AMD 라이젠7-6세대 9700X (그래니트 릿지)",
+        price: "₩539,990",
+        image: "./img/product06.png",
+        desc: [
+            "AM5",
+            "8코어 / 16스레드",
+            "DDR5",
+            "탑재 (AMD 라데온 그래픽)",
+            "Zen 5",
+            "TSMC 4nm",
+            "3.8GHz",
+            "5.5GHz",
+            "8MB",
+            "32MB",
+            "65W",
+            "88W",
+            "PCIe 5.0",
+            "5600MHz",
+            "SMT(하이퍼스레딩), AMD Ryzen Master",
+            "미포함",
+            "2260점",
+            "21436점",
+            "$359 (VAT 별도)"
+        ]
+    },
+    "h": {
+        name: "AMD 라이젠7-6세대 9800X3D (그래니트 릿지)",
+        price: "₩539,990",
+        image: "./img/product07.png",
+        desc: [
+            "AM5",
+            "8코어 / 16스레드",
+            "DDR5",
+            "탑재 (AMD 라데온 그래픽)",
+            "Zen 5",
+            "TSMC 4nm",
+            "3.8GHz",
+            "5.5GHz",
+            "8MB",
+            "32MB",
+            "65W",
+            "88W",
+            "PCIe 5.0",
+            "5600MHz",
+            "SMT(하이퍼스레딩), AMD Ryzen Master",
+            "미포함",
+            "2260점",
+            "21436점",
+            "$359 (VAT 별도)"
+        ]
+    },
+    "i": {
+        name: "인텔 코어i7-14세대 14700K (랩터레이크 리프레시)",
+        price: "₩552,890",
+        image: "./img/product08.png",
+        desc: [
+            "AM5",
+            "8코어 / 16스레드",
+            "DDR5",
+            "탑재 (AMD 라데온 그래픽)",
+            "Zen 5",
+            "TSMC 4nm",
+            "3.8GHz",
+            "5.5GHz",
+            "8MB",
+            "32MB",
+            "65W",
+            "88W",
+            "PCIe 5.0",
+            "5600MHz",
+            "SMT(하이퍼스레딩), AMD Ryzen Master",
+            "미포함",
+            "2260점",
+            "21436점",
+            "$359 (VAT 별도)"
+        ]
+    },
+    "j": {
+        name: "인텔 코어 울트라5 시리즈2 225F (애로우레이크)",
+        price: "₩326,990",
+        image: "./img/product09.png",
+        desc: [
+            "소켓1851",
+            "P6+E4코어 / 10스레드",
+            "DDR5",
+            "미탑재",
+            "TSMC 3nm",
+            "3.3GHz",
+            "4.9GHz",
+            "22MB",
+            "20MB",
+            "65W",
+            "121W",
+            "PCIe 5.0, 4.0",
+            "6400MHz",
+            "인텔 딥러닝부스트",
+            "인텔 기본쿨러 포함",
+            "$221 (VAT 별도)"
+        ]
+    },
+
+
+    // 추가 제품은 같은 방식으로 추가
 };
 
 function renderProducts() {
@@ -35,22 +263,25 @@ function renderProducts() {
     if (!container) return;
     container.innerHTML = '';
     Object.values(products).forEach(product => {
+        const descHtml = product.desc.join(" / ");
+        const badgeHtml = product.badge
+            ? `<span class="product-badge">${product.badge}</span>`
+            : "";
         const html = `
             <div class="product-list-item">
                 <div class="product-image">
                     <img src="${product.image}" alt="${product.name}">
                 </div>
                 <div class="product-main-info">
-                    <div class="product-title">${product.name}</div>
+                    <div class="product-title">
+                        ${product.name}
+                        ${badgeHtml}
+                    </div>
+                    <div class="product-desc">${descHtml}</div>
+                    <button class="product-list-more">보기</button>
+                </div>
+                <div class="product-price-box">
                     <div class="product-price">${product.price}</div>
-                    <table class="product-spec-table">
-                        ${product.specs.map(spec => `
-                            <tr>
-                                <th>${spec.항목}</th>
-                                <td>${spec.세부사항}</td>
-                            </tr>
-                        `).join('')}
-                    </table>
                 </div>
             </div>
         `;
